36ab396d2d3092611c50204ae908bb7d68416050
Anar Seyf <anar.seyf@gmail.com>
2022-07-25 19:49:45 -0700

    Parse races, standings

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-const callback = (data) => {
-  console.log("Data", data);
+window.onload = async () => {
+  d3.select("#random").text(Math.round(Math.random() * 100000));
+
+  const races = await d3.csv("./data/races.csv", parseRow);
+  const standings = await d3.csv("./data/driver_standings.csv", parseRow);
+
+  console.log("Data >> races", races);
+  console.log("Data >> standings", standings);
+
+  const lastRacesRollup = d3.rollup(races, lastRace, (d) => d.year);
+  const lastRaces = [...lastRacesRollup.values()];
+
+  console.log("Last races by year", lastRaces);
+
+  const lastRaceIds = lastRaces.map((r) => r.raceId);
+
+  const leaders = standings.filter((s) => lastRaceIds.includes(s.raceId));
+
+  console.log("end-of-season standings", leaders);
 };
 
-window.onload = async () => {
-  const data = await d3.csv("./data/circuits.csv");
-  callback(data);
+const TextRows = {
+  name: true,
+  date: true,
+  time: true,
+  url: true,
+  positionText: true,
 };
+
+function parseRow(d) {
+  const r = {};
+  Object.entries(d).forEach(([k, v]) => {
+    r[k] = TextRows[k] ? v : +v;
+  });
+  return r;
+}
+
+function lastRace(races) {
+  const maxRound = Math.max(...races.map((r) => r.round));
+  return races.find((r) => r.round === maxRound);
+}
